f9fc4268369e6d21c6ad3c9904e34b0daa2dc9ba
ARS <xxx@gmail.com>
2023-06-29 14:32:23 -0600

    Listar los datos

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -5,9 +5,21 @@ export default async function handler(req, res){
     //hacemos la peticion para pedir productos
     switch(req.method){
         case 'GET':
-            return res.status(200).json('obteniendo productos')
+            return await getProducts(req, res);
         case 'POST':
-            const {name, description, price} = req.body
+            return await saveProduct(req, res);
+            
+    }
+}
+
+const getProducts = async (req, res) => {
+    const [result] = await pool.query('SELECT * FROM product')    
+            console.log(result);
+            return res.status(200).json(result);
+}
+
+const saveProduct = async (req, res) =>{
+    const {name, description, price} = req.body
 
             const [result] = await pool.query('INSERT INTO product SET ?',{
                 name: name,
@@ -15,5 +27,4 @@ export default async function handler(req, res){
                 price: price
             });
             return res.status(200).json({name,description, price, id:result.insertId})
-    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
+import axios from "axios";
 import { ProductForm } from "../components/ProductForm";
 
-function HomePage() {
+function HomePage({products}) {
   return (
     <div>
       <ProductForm/>
+      {products.map(product => (
+        <div key={product.id}>
+          <h1>{product.name}</h1>
+          <h1>{product.description}</h1>
+          <h1>{product.price}</h1>
+        </div>
+      ))}
     </div>
   )
 }
+
+export const getServerSideProps = async (context) => {
+  const {data: products} = await axios.get("http://localhost:3000/api/products");
+
+
+  return{
+    props: {
+      products
+    },
+  };
+};
 
 export default HomePage;
